bc68cfd22a030e48100353c9a9cad28f78fafc79
Maxim Bilan <maximb.mail@gmail.com>
2015-09-11 16:20:42 +0300

    Added crosswords generator class

---
+++ crosswords_generator/CrosswordsGenerator.swift
@@ -0,0 +1,9 @@
+//
+//  CrosswordsGenerator.swift
+//  crosswords_generator
+//
+//  Created by Maxim Bilan on 9/11/15.
+//  Copyright © 2015 Maxim Bilan. All rights reserved.
+//
+
+import Foundation
