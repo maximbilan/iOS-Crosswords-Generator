829a4ea4b876c61aff8db22f062a276a466f89e3
Maxim Bilan <maximb.mail@gmail.com>
2015-09-11 16:17:57 +0300

    Added project template

---
+++ crosswords_generator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  crosswords_generator
+//
+//  Created by Maxim Bilan on 9/11/15.
+//  Copyright © 2015 Maxim Bilan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
